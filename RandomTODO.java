da7110af6ed09ec9ea7390a61f2622e358176ccd
Jules Ruz <columbus.knot@gmail.com>
2020-07-10 17:19:55 +0800

    Added/edited functionalities.

--- RandomTODO.java
+++ RandomTODO.java
@@ -82,36 +82,97 @@ public class RandomTODO {
 			System.out.print("What do you want to delete? : ");
 			String input = input_scan.nextLine();
 			if (!activities_roster.contains(input)) {
-				System.out.println("That's not on the list yo.");
+				System.out.println("That's not on the list yo.\n");
 			} else {
 				System.out.println("Are you sure you want to delete '" + input + "' from the list?");
 				System.out.println("[Y] Yes\t\t\t[N] No");
+				System.out.print("Choice: ");
 				String choice = choice_scan.nextLine();
 
 				if (choice.equals("y") || choice.equals("Y")) {
 					activities_roster.remove(input);
-					System.out.println("Successfully removed '" + input + "' from the list!");
+					System.out.println("Successfully removed '" + input + "' from the list!\n");
 					finished = true;
 				} else if (choice.equals("n") || choice.equals("N")) {
-					System.out.println("Alright, returning...");
+					System.out.println("Alright, returning...\n");
 				} else {
-					System.out.println("Pick a legit choice yo.");
+					System.out.println("Pick a legit choice yo.\n");
 				}
 
 			}
 		}
 	}
 
+	// Displays a random suggestion for what to do.
+	public void random_pick() {
+
+		Scanner rnd_picker = new Scanner(System.in);
+		Scanner choice_scan = new Scanner(System.in);
+		boolean satisfied = false;
+
+		while (!satisfied) {
+			display_activities();
+
+			if (activities_roster.isEmpty()) {
+				break; 
+			}
+
+			System.out.println("\nShall we pick one?");
+			System.out.println("[Y] Yes\t\t\t[N] No");
+
+			System.out.print("Choice: ");
+			String choice = choice_scan.nextLine();
+
+			if (choice.equals("y") || choice.equals("Y")) {
+
+				System.out.println("\nPicking...\n");
+
+				Random suggestor = new Random(System.currentTimeMillis());
+				int list_index = suggestor.nextInt(activities_roster.size());
+
+				System.out.println("Today I suggest you: " + activities_roster.get(list_index) + "\n");
+
+			} else if (choice.equals("n") || choice.equals("N")) {
+				System.out.println("Alright, returning...\n");
+				break;
+			} else {
+				System.out.println("Pick a legit choice yo.\n");
+			}
+
+			System.out.println("Want to pick again?");
+			System.out.println("[Y] Yes\t\t\t[N] No");
+
+			System.out.print("Choice: ");
+			String repick = choice_scan.nextLine();
+
+			if (repick.equals("y") || repick.equals("Y")) {
+
+				System.out.println("Alright hold on...\n");
+
+			} else if (repick.equals("n") || repick.equals("N")) {
+				System.out.println("Thank you for picking! Going back to main menu...\n");
+				satisfied = true;
+			} else {
+				System.out.println("Pick a legit choice yo.\n");
+			}
+
+		}
+
+	}
+
+	// Prints menu
 	public void print_menu() {
 		System.out.println("===== RANDOM TO-DO =====");
 		System.out.println("For when you feel like shit\nbut still want to be productive\n");
-		System.out.println("[1] Add a thing to do");
-		System.out.println("[2] Delete a thing");
-		System.out.println("[3] See all the stuff you can do");
+		System.out.println("[1] Suggest me a random thing!!");
+		System.out.println("[2] Add a thing to do");
+		System.out.println("[3] Delete a thing");
+		System.out.println("[4] See all the stuff you can do");
 		System.out.println("[0] Exit");
 		System.out.print("What do you want to do? : ");
 	}
 
+	// Navigates the menu. You can include the print_menu code here, but I prefer not to. Looks cleaner for me.
 	public void navigate_menu() {
 
 		Scanner picker = new Scanner(System.in);
@@ -122,15 +183,18 @@ public class RandomTODO {
 				choice = picker.nextLine();
 				int option = Integer.valueOf(choice);
 				switch (option) {
-					case 1: add_stuff_todo();
+					case 1: random_pick();
+							print_menu();
+							break;
+					case 2: add_stuff_todo();
 							print_menu();
 							break;
 	
-					case 2: delete_thing_to_do();
+					case 3: delete_thing_to_do();
 							print_menu();
 							break;
 
-					case 3: display_activities();
+					case 4: display_activities();
 							print_menu();
 							break;
 	
